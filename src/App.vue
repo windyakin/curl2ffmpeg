7726ef40847850901e2fb921ecf22b7ebacb84c0
windyakin <windyakin@gmail.com>
2021-09-29 07:51:42 +0900

    :left_right_arrow: Fix grid

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container">
     <div class="row justify-content-center my-4">
-      <div class="col col-8">
+      <div class="col col-12 col-lg-8">
         <h1>curl2ffmpeg</h1>
         <a href="https://github.com/windyakin/curl2ffmpeg">GitHub Repository</a>
         <CommandCoverter />
       </div>
     </div>
     <div class="row justify-content-center my-4">
-      <div class="col col-8 text-end">
+      <div class="col col-12 col-lg-8 text-end">
         &copy; 2021 windyakin
       </div>
     </div>
