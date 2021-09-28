96912d61fc282fefbe68476d2d426b33f838600e
windyakin <windyakin@pepabo.com>
2021-09-28 12:34:43 +0900

    :purple_heart: Fix base dir

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/curl2ffmpeg/',
   build: {
     sourcemap: true,
-    minify: false
+    minify: false,
   },
   resolve: {
     alias: {
